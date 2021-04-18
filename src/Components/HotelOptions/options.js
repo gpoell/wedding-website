2e17c1a59d6522f78dd036f25e6a09fe0b14b99a
gpoell <gpoell.gp@gmail.com>
2021-04-18 16:56:20 -0400

    updating accommodations due to changes in some hotels

--- src/Components/HotelOptions/options.js
+++ src/Components/HotelOptions/options.js
@@ -1,10 +1,10 @@
 import imgMitchellsLodge from "../../Images/Accommodations/mitchell_lodge.jpg";
 import imgHighlandInnLodge from "../../Images/Accommodations/highlands-inn-lodge.jpg";
-import img200Main from "../../Images/Accommodations/200-main.jpg";
+// import img200Main from "../../Images/Accommodations/200-main.jpg";
 import imgParkMain from "../../Images/Accommodations/park-on-main.jpg";
-import imgMainStInn from "../../Images/Accommodations/main-street-inn.jpg";
 import imgOldEdwards from "../../Images/Accommodations/old-edwards-inn.jpg";
-import imgChandlerInn from "../../Images/Accommodations/the-chandler-inn.jpg";
+import imgWellsHotel from "../../Images/Accommodations/wellshotel.jpg";
+import imgHighlanderMountain from "../../Images/Accommodations/highlandermountainhouse.jpg";
 
 export const options = [
   {
@@ -27,16 +27,16 @@ export const options = [
     phone: "(828) 526-5899",
     notes: "Call for reservation and rates"
   },
-  {
-    hotel: "200 Main",
-    location: "200 Main St, Highlands, NC 28741",
-    distance: "11 min | 0.5 miles",
-    price: 185,
-    website: "https://www.200main.com/",
-    image: img200Main,
-    phone: "(855) 271-2809",
-    notes: "Call for room block reservation"
-  },
+  // {
+  //   hotel: "200 Main",
+  //   location: "200 Main St, Highlands, NC 28741",
+  //   distance: "11 min | 0.5 miles",
+  //   price: 185,
+  //   website: "https://www.200main.com/",
+  //   image: img200Main,
+  //   phone: "(855) 271-2809",
+  //   notes: "Call for room block reservation"
+  // },
   {
     hotel: "The Park On Main",
     location: "205 Main St, Highlands, NC 28741",
@@ -48,12 +48,12 @@ export const options = [
     notes: ""
   },
   {
-    hotel: "Main Street Inn",
+    hotel: "Highlander Mountain House",
     location: "270 Main St, Highlands, NC 28741",
     distance: "8 min | 0.4 miles",
     price: 215,
-    website: "https://www.mainstreet-inn.com/en-us",
-    image: imgMainStInn,
+    website: "https://highlandermountainhouse.com/",
+    image: imgHighlanderMountain,
     phone: "(828) 526-2590",
     notes: ""
   },
@@ -68,12 +68,12 @@ export const options = [
     notes: "Call for room block reservation"
   },
   {
-    hotel: "The Chandler Inn",
+    hotel: "The Wells Hotel",
     location: "790 N 4th St, Highlands, NC 28741",
     distance: "14 min | 0.7 miles",
     price: 200,
-    website: "https://www.thechandlerinn.com/",
-    image: imgChandlerInn,
+    website: "https://www.thewellshotel.com/",
+    image: imgWellsHotel,
     phone: "(828) 482-7736",
     notes: ""
   }
